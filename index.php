6a062afc6ebc8719252da6024f7626acc9ac82e4
Angelo <ghelomarquez@gmail.com>
2022-05-16 03:36:33 +0800

    Complete Home Page UI Update

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Library Monitoring System</title>
+    <link rel="stylesheet" href="/LibraryInformationMonitoringSystem/stylesheets/main.css">
+</head>
+<body>
+    <div class="Header">
+        <div class="headerbg">
+            <div class="Logo"><a href="/LibraryInformationMonitoringSystem/index.php"><img src="/LibraryInformationMonitoringSystem/assets.img/MMPNS-Logo.png" alt=""></a>
+                <div class="Header Text">
+                    <span class="HH1"><h1>Madre Maria Pia Notari School
+                    <span class="HH2"> Library Monitoring System</span></h1></span>
+                </div>
+            </div>
+            <div class="RightOptions">
+                <ul class="Menu">
+                    <li><button class="Button">Login</button></li>
+                    <li><a href="../LibraryInformationMonitoringSystem/inventory.html">Inventory</a></li>
+                    <li><a href="../LibraryInformationMonitoringSystem/records.html">Records</a></li>
+                    <li><a href="../LibraryInformationMonitoringSystem/index.php">Home</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="Wrapper">
+        <div class="hero">
+            <div class="homeText">
+                <div class="line1">
+                    Welcome to
+                </div>
+                <div class="line2">
+                    Madre Maria Pia Notari School's
+                </div>
+                <div class="line3">
+                    Library Monitoring System
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
