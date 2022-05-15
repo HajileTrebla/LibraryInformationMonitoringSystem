a21a5f98206e1a334595e749007e29d1610e7664
Eli Q <eli.quinio17@gmail.com>
2022-05-16 05:07:43 +0800

    Fix UI Scrolling

--- index.php
+++ index.php
@@ -1,24 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Library Monitoring System</title>
     <link rel="stylesheet" href="/LibraryInformationMonitoringSystem/stylesheets/main.css">
 </head>
+
 <body>
     <div class="Header">
         <div class="headerbg">
             <div class="Logo"><a href="/LibraryInformationMonitoringSystem/index.php"><img src="/LibraryInformationMonitoringSystem/assets.img/MMPNS-Logo.png" alt=""></a>
                 <div class="Header Text">
-                    <span class="HH1"><h1>Madre Maria Pia Notari School
-                    <span class="HH2"> Library Monitoring System</span></h1></span>
+                    <span class="HH1">
+                        <h1>Madre Maria Pia Notari School
+                            <span class="HH2"> Library Monitoring System</span>
+                        </h1>
+                    </span>
                 </div>
             </div>
             <div class="RightOptions">
                 <ul class="Menu">
                     <li><button class="Button">Login</button></li>
-                    <li><a href="../LibraryInformationMonitoringSystem/about.html">About</a></li>
+                    <li><a href="#about">About</a></li>
                     <li><a href="../LibraryInformationMonitoringSystem/inventory.html">Inventory</a></li>
                     <li><a href="../LibraryInformationMonitoringSystem/records.html">Records</a></li>
                     <li><a href="../LibraryInformationMonitoringSystem/index.php">Home</a></li>
@@ -41,6 +46,7 @@
             </div>
         </div>
     </div>
-    
+
 </body>
+
 </html>
