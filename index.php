0648eeba8d691a4e83ee3c2b04f58de8985c4df7
Eli Q <eli.quinio17@gmail.com>
2022-05-16 06:03:56 +0800

    Add Modal Log In (Nonfunctionalw)

--- index.php
+++ index.php
@@ -9,44 +9,73 @@
 </head>
 
 <body>
-    <div class="Header">
-        <div class="headerbg">
-            <div class="Logo"><a href="/LibraryInformationMonitoringSystem/index.php"><img src="/LibraryInformationMonitoringSystem/assets.img/MMPNS-Logo.png" alt=""></a>
-                <div class="Header Text">
-                    <span class="HH1">
-                        <h1>Madre Maria Pia Notari School
-                            <span class="HH2"> Library Monitoring System</span>
-                        </h1>
-                    </span>
+    <div class="container">
+        <div class="Header">
+            <div class="headerbg">
+                <div class="Logo"><a href="/LibraryInformationMonitoringSystem/index.php"><img src="/LibraryInformationMonitoringSystem/assets.img/MMPNS-Logo.png" alt=""></a>
+                    <div class="Header Text">
+                        <span class="HH1">
+                            <h1>Madre Maria Pia Notari School
+                                <span class="HH2"> Library Monitoring System</span>
+                            </h1>
+                        </span>
+                    </div>
                 </div>
-            </div>
-            <div class="RightOptions">
-                <ul class="Menu">
-                    <li><button class="Button">Login</button></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="../LibraryInformationMonitoringSystem/inventory.html">Inventory</a></li>
-                    <li><a href="../LibraryInformationMonitoringSystem/records.html">Records</a></li>
-                    <li><a href="../LibraryInformationMonitoringSystem/index.php">Home</a></li>
-                </ul>
+                <div class="RightOptions">
+                    <ul class="Menu">
+                        <!--Login changes to Logout when logged into session, Inventory and Records are also hidden when logged out-->
+                        <li><button data-modal-targer="#login_modal" class="Button">Login</button></li>
+                        <div class="modal" id="login_modal">
+                            <div class="modal-header">
+                                <div class="title">login
+                                </div>
+                                <button data-close-button class="close-button">&times;</button>
+                            </div>
+                            <div class="modal-body">
+                                <div class="sign-up-form">
+                                    <form action="php.functions/login.inc.php" method="POST">
+                                        <input type="text" name="name" placeholder="Username/Email...">
+                                        <input type="password" name="pwd" placeholder="Password...">
+                                        <button type="submit" name="submit">Log In</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <div id="overlay"></div>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="../LibraryInformationMonitoringSystem/inventory.php">Inventory</a></li>
+                        <li><a href="../LibraryInformationMonitoringSystem/records.php">Records</a></li>
+                        <li><a href="../LibraryInformationMonitoringSystem/index.php">Home</a></li>
+                    </ul>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="Wrapper">
-        <div class="hero">
-            <div class="homeText">
-                <div class="line1">
-                    Welcome to
-                </div>
-                <div class="line2">
-                    Madre Maria Pia Notari School's
-                </div>
-                <div class="line3">
-                    Library Monitoring System
+        <div class="Wrapper">
+            <div class="hero">
+                <div class="homeText">
+                    <div class="line1">
+                        Welcome to
+                    </div>
+                    <div class="line2">
+                        Madre Maria Pia Notari School's
+                    </div>
+                    <div class="line3">
+                        Library Monitoring System
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
+            <!--Borrow to be hidden behind login requirements-->
+            <div class="Wrapper">
+                <div class="Borrow Form Header">
+                    borrow form head
+                </div>
+                <div class="Borrow Form">
+                    borrow form
+                </div>
+            </div>
 
+        </div>
+    </div>
 </body>
 
 </html>
