9432d89427e0943a3395419c8e40c8bc0012f1e8
Eli Q <eli.quinio17@gmail.com>
2022-06-24 12:20:46 +0800

    Created borrow.php

---
+++ borrow.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<?php
+session_start();
+if (!isset($_SESSION["useruid"])) {
+    header("Location: index.php?redirect");
+    exit();
+}
+?>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Library Monitoring System</title>
+
+    <link rel="stylesheet" href="/LibraryInformationMonitoringSystem/stylesheets/main.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Grandstander:wght@600&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500&display=swap" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+    <script src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.15/js/dataTables.bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.4/css/bootstrap-datepicker.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.4/js/bootstrap-datepicker.js"></script>
+
+</head>
+
+<body>
+    <div class="container">
+        <div class="Header">
+            <div class="headerbg">
+                <div class="Logo"><a href="/LibraryInformationMonitoringSystem/index.php"><img src="/LibraryInformationMonitoringSystem/assets.img/MMPNS-Logo.png" alt=""></a>
+                    <div class="Header Text">
+                        <span class="HH1">
+                            <h1>Madre Maria Pia Notari School
+                                <span class="HH2"> Library Monitoring System</span>
+                            </h1>
+                        </span>
+                    </div>
+                </div>
+                <div class="RightOptions">
+                    <ul class="Menu">
+                        <?php
+                        if (isset($_SESSION["useruid"])) {
+                            echo '<li><a href="../LibraryInformationMonitoringSystem/php.functions/logout.inc.php" class="button" id="Logout-button">Logout</a></li>';
+                        } else {
+                            echo '<li><a href="#" class="button" id="Login-button">Login</a></li>';
+                        }
+                        ?>
+                        <li><a href="../LibraryInformationMonitoringSystem/about.php">About</a></li>
+                        <?php
+                        if (isset($_SESSION["useruid"])) {
+                            echo '<li><a href="../LibraryInformationMonitoringSystem/inventory.php">Inventory</a></li>';
+                            echo '<li><a href="../LibraryInformationMonitoringSystem/records.php">Records</a></li>';
+                        }
+                        ?>
+                        <li><a href="../LibraryInformationMonitoringSystem/index.php">Home</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="Wrapper">
+            <div class="hero">
+                <div class="mainText">
+                    <div class="line2">
+                        Library Inventory
+                    </div>
+                </div>
+                <div class="select-area">
+                    <div class="table-responsive">
+                        <br />
+                        <div class="row">
+                            <div class="input-text">
+                                <div class="col-md-2">
+                                    <label for="subj_categ">Category</label>
+                                    <select name="subj_categ" id="subj_categ">
+                                        <option value="All">All</option>
+                                        <?php
+                                        require_once "./php.functions/dbh.inc.php";
+
+                                        $dbConn = getConn();
+
+                                        $sql = "SELECT * FROM lib_inventory_subjects_category;";
+
+                                        $result = pg_query($dbConn, $sql);
+                                        if (!$result) {
+                                            echo "An Error Occured!\n";
+                                            exit();
+                                        }
+
+                                        while ($categName = pg_fetch_row($result)) {
+                                            echo "<option value='$categName[1]'>$categName[1]</option>";
+                                            echo "<br />";
+                                        }
+                                        ?>
+                                    </select>
+                                </div>
+                                <div class="col-md-2">
+                                    <input type="button" name="search" id="search" value="Search" class="btn btn-info" />
+                                </div>
+
+                            </div>
+                        </div>
+                        <br />
+                        <table id="inventory_data" class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Resource ID</th>
+                                    <th>Resource Name</th>
+                                    <th>Authors/Publisher</th>
+                                    <th>Subject</th>
+                                    <th>Quantity</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="Footer">
+            <div class="left-bar">
+                <div>
+                    ADD EXTRA INFO HERE/LINKS
+                </div>
+            </div>
+            <div class="right-bar">
+                <div>
+                    ADD EXTRA INFO/CONTACTS HERE
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+<script src="./js/inventory.jsx"></script>
+
+</html>
