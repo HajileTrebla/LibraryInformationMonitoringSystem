63e623fbcf8351461fb2e1d61817f9c36184bbf3
Angelo <ghelomarquez@gmail.com>
2022-06-27 02:36:13 +0800

    Record Buttons

--- records.php
+++ records.php
@@ -1,16 +1,28 @@
 <!DOCTYPE html>
+<?php
+session_start();
+?>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Library Monitoring System</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/LibraryInformationMonitoringSystem/stylesheets/main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Grandstander:wght@600&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500&display=swap" rel="stylesheet">
 </head>
 
 <body>
     <div class="container">
-        <div class="Header">
+        <div class="Header" id="topPage">
             <div class="headerbg">
                 <div class="Logo"><a href="/LibraryInformationMonitoringSystem/index.php"><img src="/LibraryInformationMonitoringSystem/assets.img/MMPNS-Logo.png" alt=""></a>
                     <div class="Header Text">
@@ -23,11 +35,21 @@
                 </div>
                 <div class="RightOptions">
                     <ul class="Menu">
-                        <!--Login changes to Logout when logged into session, Inventory and Records are also hidden when logged out-->
+                        <?php
+                        if (isset($_SESSION["useruid"])) {
+                            echo '<li><a href="../LibraryInformationMonitoringSystem/php.functions/logout.inc.php" class="button" id="Logout-button">Logout</a></li>';
+                        } else {
+                            echo '<li><a href="#" class="button" id="Login-button">Login</a></li>';
+                        }
+                        ?>
 
                         <li><a href="../LibraryInformationMonitoringSystem/about.php">About</a></li>
-                        <li><a href="../LibraryInformationMonitoringSystem/inventory.php">Inventory</a></li>
-                        <li><a href="../LibraryInformationMonitoringSystem/records.php">Records</a></li>
+                        <?php
+                        if (isset($_SESSION["useruid"])) {
+                            echo '<li><a href="../LibraryInformationMonitoringSystem/inventory.php">Inventory</a></li>';
+                            echo '<li><a href="../LibraryInformationMonitoringSystem/records.php">Records</a></li>';
+                        }
+                        ?>
                         <li><a href="../LibraryInformationMonitoringSystem/index.php">Home</a></li>
                     </ul>
                 </div>
@@ -35,27 +57,40 @@
         </div>
         <div class="Wrapper">
             <div class="hero">
-                <div class="recordsText">
-                    <div class="line1">
+                <div class="popup">
+                    <div class="login">
+                        <div class="loginForm">
+                            <div class="loginHeader">LOGIN
+                            </div>
+                            <div><img src="/LibraryInformationMonitoringSystem/assets.img/closebutton.png" alt="X" class="close">
+                            </div>
+                            <div class="loginText">
+                                <form action="php.functions/login.inc.php" method="POST">
+                                    <input type="text" name="uid" placeholder="Username">
+                                    <input type="password" name="pwd" placeholder="Password">
+                                    <a href=""><button type="submit" class="submit" name="Login">LOGIN</button></a>
+                                </form>
+                            </div>
+                        </div>
                     </div>
-                    <div class="line2">
-                    </div>
-                    <div class="line3">
-                    </div>
+                    <div class="overlay"></div>
+                </div>
+                <div class="Wrapper">
+                    <ul class="recButtons">
+                        <li><a href=""><button type="button" class="recButton" name="fullInventory-button">Full Inventory</button></a></li>
+                        <li><a href=""><button type="button" class="recButton" name="transactions-button">Transactions</button></a></li>
+                        <li><a href=""><button type="button" class="recButton" name="visitorLog-button">Visitor Log</button></a></li>
+                        <li><a href=""><button type="button" class="recButton" name="inventoryChangeLog-button">Inventory Changelog</button></a></li>
+                        <li><a href=""><button type="button" class="recButton" name="masterList-button">Master List</button></a></li>
+                        <li><a href=""><button type="button" class="recButton" name="globalLog-button">Global Log</button></a></li>
+                    </ul>
                 </div>
             </div>
-            <!--Borrow to be hidden behind login requirements-->
-            <div class="Wrapper">
-                <div class="Borrow Form Header">
-                    borrow form head
-                </div>
-                <div class="Borrow Form">
-                    borrow form
-                </div>
-            </div>
-
+        </div>
+        <div class="Footer">
         </div>
     </div>
 </body>
+<script src="./js/index.jsx"></script>
 
 </html>
